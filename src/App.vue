2d90d96b918689a5fa0f507791a1e383154c0865
EliaPennoni <elia.pennoni1991@gmail.com>
2024-09-17 15:22:48 +0200

    creato template base per esercizio boolflix

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script>
+import axios from "axios";
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+import AppFooter from "./components/AppFooter.vue";
+export default {
+  data() {
+    return {
+  
+    };
+  },
+  created() {
+    axios
+      
+  },
+  computed: {
+    
+  },
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter
+  },
+};
+
+</script>
+
+<template>
+    <AppHeader />
+
+    <AppMain />
+
+    <AppFooter />
+</template>
+
+<style lang="scss" scoped>
+    @import "bootstrap/scss/bootstrap";
+</style>
